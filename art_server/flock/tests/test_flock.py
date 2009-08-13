56a3af3cfcda3ffeae16653e0cc69863bd99672d
Trevor F. Smith <trevor@trevor.smith.name>
2009-08-13 16:04:38 -0700

    Added a quick test for the tests.

--- art_server/flock/tests/test_flock.py
+++ art_server/flock/tests/test_flock.py
@@ -25,12 +25,18 @@ class BasicViewsTest(TestCase):
 		self.failUnless(StatusListener.objects.all().count() == 1)
 		sl = StatusListener.objects.all()[0]
 		self.failUnlessEqual(sl.host, '127.0.0.1:8088')
+		self.failUnlessEqual(len(sl.tests.all()), 0)
 		
 		response = self.client.post(APP_PATH, {'unregister':8088})
 		self.failUnlessEqual(response.status_code, 200, 'status was %s' % response.status_code )
 		self.failUnless('flock/status.html' in [template.name for template in response.template])
 		self.failUnless(StatusListener.objects.all().count() == 0)
 
+		response = self.client.post(APP_PATH, {'register':8088, 'tests':'test1,test2,test3'})
+		self.failUnlessEqual(response.status_code, 200, 'status was %s' % response.status_code )
+		self.failUnless('flock/status.html' in [template.name for template in response.template])
+		self.failUnless(StatusListener.objects.all().count() == 1)
+		sl = StatusListener.objects.all()[0]
+		self.failUnlessEqual(len(sl.tests.all()), 3)
 		
-
 
