435e644292c33dbe9ebc726608b5e9cce3d8e880
Trevor F. Smith <trevor@trevor.smith.name>
2010-04-14 14:41:11 -0700

    changed the bacrp command and set the port environment variable

--- art_server/lighting/bacnet_control.py
+++ art_server/lighting/bacnet_control.py
@@ -13,10 +13,11 @@ import logging
 USAGE_MESSAGE = 'usage: bacnet_control <read-ao|write-ao> <device id> <property id> [<value>]'
 
 class BacnetControl:
-	def __init__(self, bin_dir_path):
+	def __init__(self, bin_dir_path, bacnet_port=47809):
 		self.bin_dir_path = bin_dir_path
-
+		self.bacnet_port = bacnet_port
 	def run_command(self, args):
+		os.environ['BACNET_IP_PORT'] = '%s' % self.bacnet_port
 		proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 		output = ''
 		while True:
@@ -38,14 +39,14 @@ class BacnetControl:
 		bin_path = self.get_bin_path('bacrp')
 		# bacrp device-instance object-type object-instance property [index]
 		# bacrp 100 1 23 1 85
-		args = [bin_path, '%s' % int(device_id), '1', '%s' % int(property_id), '85']
+		args = [bin_path, '%s' % int(device_id), '2', '%s' % int(property_id), '85']
 		return self.run_command(args)
 
 	def write_analog_output_int(self, device_id, property_id, value):
 		"""Returns the Present-Value of an Analog Output property"""
 		bin_path = self.get_bin_path('bacwp')
 		# bacwp device-instance object-type object-instance property priority index tag value [tag value...]
-		args = [bin_path, '%s' % int(device_id), '1', '%s' % int(property_id), '85', '16', '-1', '2', '%s' % int(value)]
+		args = [bin_path, '%s' % int(device_id), '2', '%s' % int(property_id), '85', '16', '-1', '2', '%s' % int(value)]
 		return self.run_command(args)
 
 def main():
