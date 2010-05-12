c0307d341eb299f050ca184e56b32f3057667a4f
Trevor F. Smith <trevor@trevor.smith.name>
2010-05-12 10:58:56 -0700

    Now the ArtCam task will send an alert if it can not update an artcam.

--- art_server/artcam/tasks.py
+++ art_server/artcam/tasks.py
@@ -7,7 +7,7 @@ import sys
 
 class ArtcamTask(Task):
 	"""The schedule task which updates the stored images for each of the artcams."""
-	def __init__(self, loopdelay=300, initdelay=15):
+	def __init__(self, loopdelay=300, initdelay=1):
 		Task.__init__(self, self.do_it, loopdelay, initdelay)
 	def do_it(self):
 		from django.contrib.sites.models import Site
@@ -16,7 +16,10 @@ class ArtcamTask(Task):
 		for artcam in Artcam.objects.all():
 			try:
 				artcam.update_photo()
+				print "updated photo"
 			except:
+				print 'error in artcam task: '
+				self.send_alert("ArtCam Failure: %s" % artcam, "Could not update %s: %s %s" % (artcam, sys.exc_info()[0], sys.exc_info()[1]))
 				logging.debug("Could not update %s: %s %s", artcam, sys.exc_info()[0], sys.exc_info()[1])
 
 
